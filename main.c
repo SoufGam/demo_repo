0440168542d24a558a981fc7ed868bd0007b44c1
Soufien Gamgam <sgamgam@gmail.com>
2024-10-09 19:51:19 +0200

    Creat for loop and array

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-
+char name[20];
 void fun(void);
-
+int count = 20;
 int main(void)
 {
   fun();
+  for (size_t i = 0; i < count; i++)
+  {
+    printf("%c\n", name[i]);
+  }
+
   return 0;
 }
 
